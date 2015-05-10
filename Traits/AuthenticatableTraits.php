6007643085d5f1283deff13869caae04c805788c
Christopher Eklund <eklundchristopher@gmail.com>
2015-05-10 15:14:46 +0200

    Organised contracts

---
+++ Traits/AuthenticatableTraits.php
@@ -0,0 +1,30 @@
+<?php namespace Apolune\Core\Traits;
+
+trait AuthenticatableTraits {
+
+	/**
+	 * Get the token value for the "remember me" session.
+	 *
+	 * @param  string  $column
+	 * @return string
+	 */
+	public function getRememberToken($column)
+	{
+		return $this->{$column};
+	}
+
+	/**
+	 * Set the token value for the "remember me" session.
+	 *
+	 * @param  string  $column
+	 * @param  string  $value
+	 * @return void
+	 */
+	public function setRememberToken($column, $value)
+	{
+		$this->{$column} = $value;
+
+		$this->save();
+	}
+
+}
