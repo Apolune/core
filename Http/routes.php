41ca57e5a112d7fbea363fff1d077411ede9c923
Christopher Eklund <eklundchristopher@gmail.com>
2015-05-11 20:10:17 +0200

    Moved legal to support package

--- Http/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$router->group(['prefix' => '/'], function($router)
-{
-	$router->get('/terms', 'DocumentController@terms');
-	$router->get('/rules', 'DocumentController@rules');
-	$router->get('/privacy', 'DocumentController@privacy');
-});
